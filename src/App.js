20c877c97ec76371a95c44794a64888f16ac9717
Charliehexx <adi2652002@gmail.com>
2023-02-05 18:27:56 +0530

    Initial project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from <axios<
 
 function App() {
+
+  const [prompt, setPrompt] = useState("")
+
+  console.log(prompt)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
+      {/* Create an input box and button saying next beside it */}
+      <div className="flex items-center justify-center gap-4">
+        <input
+          className="border-2 border-black rounded-md p-2"
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Enter a prompt"
+        />
+        <button className="bg-black text-white rounded-md p-2">Next</button>
+      </div>
     </div>
   );
 }
